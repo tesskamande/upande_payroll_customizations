721178321cb38b17ffcb5e5cf2c394c53a4ae9ed
Teresia Kamande <tesskamande3824@gmail.com>
2025-11-21 01:08:31 +0300

    payroll

---
+++ upande_payroll_customizations/upande_payroll_customizations/report/net_pay/net_pay.js
@@ -0,0 +1,48 @@
+// Copyright (c) 2025, dev@upande.com and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["NET PAY"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"width": "100px",
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("Start Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(),-1),
+			"reqd": 1,
+			"width": "100px"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("End Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1,
+			"width": "100px"
+		},
+		{
+			"fieldname": "currency",
+			"fieldtype": "Link",
+			"options": "Currency",
+			"label": __("Currency"),
+			"default": erpnext.get_currency(frappe.defaults.get_default("Company")),
+			"width": "50px"
+		},
+		{
+			"fieldname":"docstatus",
+			"label":__("Document Status"),
+			"fieldtype":"Select",
+			"options":["Draft", "Submitted", "Cancelled"],
+			"default": "Submitted",
+			"width": "100px"
+		}
+	]
+}
