2e9a90a5868ccb0b78996f335b563acecdbedfb9
Teresia Kamande <tesskamande3824@gmail.com>
2026-01-27 18:23:35 +0300

    Payroll reports have been tried on another site

--- upande_payroll_customizations/upande_payroll_customizations/doctype/subdepartment_department_mapping/test_subdepartment_department_mapping.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, dev@upande.com and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSubdepartmentDepartmentMapping(FrappeTestCase):
-	pass
